4966faa3f0946d8a554d59e2d36de8d122560fc3
Oliver Zott <oliver.zott@agindo.at>
2024-10-21 16:07:10 +0200

    Viewmodel changed to use observableobjects class

--- maui-car-list/ViewModels/BaseViewModel.cs
+++ maui-car-list/ViewModels/BaseViewModel.cs
@@ -1,41 +1,18 @@
-﻿using System.ComponentModel;
-using System.Runtime.CompilerServices;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace maui_car_list.ViewModels;
 
 // Properties of ViewModel can be observed from application. When the changed a event is fired
-internal class BaseViewModel : INotifyPropertyChanged
+// partial cause toolkit is generating code: see Dependencies - net8.0-android-Analyzers-SourceGenerator.ObservablePropertyGenerator
+public partial class BaseViewModel : ObservableObject
 {
+    [ObservableProperty]
     string title;   // title of page we are on
+
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(isNotBusy))]
     bool isBusy;    // is page busy
 
-    public bool IsBusy
-    {
-        get => isBusy;
-        set
-        {
-            if (isBusy == value) return;
-            isBusy = value;
-            OnPropertyChanged();
-        }
-    }
+    public bool isNotBusy => !isBusy;
 
-    public string Title
-    {
-        get => title;
-        set
-        {
-            if (title == value) return;
-            title = value;
-            OnPropertyChanged();
-        }
-    }
-
-    public event PropertyChangedEventHandler? PropertyChanged;
-
-    // gets name of property which triggered this and pass it to name attribute
-    public void OnPropertyChanged([CallerMemberName] string name = null)
-    {
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
-    }
 }
---
+++ maui-car-list/ViewModels/CarListViewModel.cs
@@ -0,0 +1,48 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using maui_car_list.Models;
+using maui_car_list.Services;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+
+namespace maui_car_list.ViewModels;
+public partial class CarListViewModel : BaseViewModel
+{
+    private readonly CarService carService;
+
+    public ObservableCollection<Car> Cars { get; private set; } = []; // private set because only set here!! Also default initialized (shorthad of new ()
+
+    public CarListViewModel(CarService carService)
+    {
+        Title = "Car List";
+        this.carService = carService;
+    }
+
+    [RelayCommand]
+    async Task GetCarListAsync()
+    {
+        if (IsBusy) return;
+
+        try
+        {
+            IsBusy = true;
+            if (Cars.Any()) Cars.Clear();
+
+            var cars = carService.GetCars();
+            // foreach cause no AddRange for observablecollecrion
+            foreach (var car in cars)
+            {
+                Cars.Add(car);
+            }
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine($"Unable to get cars: {ex.Message}");  // whats the sink??
+            await Shell.Current.DisplayAlert($"Error", "Failed to retrieve list of cars", "Ok");  // could abstract that away and user via DI
+        }
+        finally
+        {
+            IsBusy = false;
+        }
+
+    }
+}
